e34519b9da555e77b21302563f0761028507857d
Janos Gyerik <janos@kronos>
2015-06-21 12:25:23 +0200

    basic app, no status yet

--- ionic/mrgenie/www/js/controllers.js
+++ ionic/mrgenie/www/js/controllers.js
@@ -1,28 +1,43 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('RoomListController', ['$scope', 'Room', function ($scope, Room) {
+        Room.getAll().success(function (data) {
+            $scope.rooms = data.results;
+        });
+    }])
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+    .controller('RoomController', function ($scope, $stateParams, Room, Reservation) {
+        function getRoom(rooms, name) {
+            for (var i = 0; i < rooms.length; ++i) {
+                var room = rooms[i];
+                if (room.name == name) {
+                    return room;
+                }
+            }
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+            if (rooms.length > 0) {
+                return rooms[0];
+            }
+        }
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+        Room.getAll().success(function (data) {
+            var rooms = data.results;
+            var room = getRoom(rooms, $stateParams.name);
+            if (!room) {
+                $scope.room = {name: '?'};
+                return;
+            }
+
+            $scope.room = room;
+
+            Reservation.query($scope.room.objectId).success(function (data) {
+                $scope.reservations = data.results
+                    .map(function (item) {
+                        var start_date = item.start_date.iso;
+                        var time = start_date.substr(start_date.indexOf('T') + 1, 5);
+                        return {'time': time};
+                    });
+            });
+        });
+    })
+;
--- ionic/mrgenie/www/js/services.js
+++ ionic/mrgenie/www/js/services.js
@@ -1,50 +1,96 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+    .factory('Chats', function () {
+        // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  },{
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+        // Some fake testing data
+        var chats = [{
+            id: 0,
+            name: 'Ben Sparrow',
+            lastText: 'You on your way?',
+            face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+        }, {
+            id: 1,
+            name: 'Max Lynx',
+            lastText: 'Hey, it\'s me',
+            face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+        }, {
+            id: 4,
+            name: 'Mike Harrington',
+            lastText: 'This is wicked good ice cream.',
+            face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+        }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+        return {
+            all: function () {
+                return chats;
+            },
+            remove: function (chat) {
+                chats.splice(chats.indexOf(chat), 1);
+            },
+            get: function (chatId) {
+                for (var i = 0; i < chats.length; i++) {
+                    if (chats[i].id === parseInt(chatId)) {
+                        return chats[i];
+                    }
+                }
+                return null;
+            }
+        };
+    })
+    .factory('Room', ['$http', 'PARSE_CREDENTIALS', function ($http, PARSE_CREDENTIALS) {
+        function getAll() {
+            return $http.get('https://api.parse.com/1/classes/Room', {
+                headers: {
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY
+                }
+            });
         }
-      }
-      return null;
-    }
-  };
-});
+        return {
+            getAll: getAll,
+            get: function (id) {
+                return $http.get('https://api.parse.com/1/classes/Room/' + id, {
+                    headers: {
+                        'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                        'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY
+                    }
+                });
+            }
+        }
+    }])
+    .factory('Reservation', ['$http', 'PARSE_CREDENTIALS', function ($http, PARSE_CREDENTIALS) {
+        function getAll() {
+            return $http.get('https://api.parse.com/1/classes/Reservation', {
+                headers: {
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY
+                }
+            });
+        }
+        return {
+            getAll: getAll,
+            query: function (objectId) {
+                return $http.get('https://api.parse.com/1/classes/Reservation', {
+                    headers: {
+                        'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                        'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY
+                    },
+                    params: {
+                        where: {
+                            room: {
+                                __type: 'Pointer',
+                                className: 'Room',
+                                objectId: objectId
+                            }
+                        }
+                    }
+                });
+            }
+        }
+    }])
+    .value('PARSE_CREDENTIALS', {
+        APP_ID: 'tdhNUXe5ytcL28D4e0hv5782fFw0N6iFkIS7Tiyn',
+        REST_API_KEY: 'aoi46j5L12B2o1pNmGzceCSTj3cyzgGb5nK8uPzA'
+    })
+;
