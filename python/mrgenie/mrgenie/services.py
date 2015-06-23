3fcbcc798a2cb83c5f4d512bd06818b61009ba54
Janos Gyerik <janos@kronos>
2015-06-23 18:30:19 +0200

    added more service api methods

--- python/mrgenie/mrgenie/services.py
+++ python/mrgenie/mrgenie/services.py
@@ -14,6 +14,25 @@ class Service:
     def get_reservations(self, room_id):
         """
         Get reservations for a room
+        :param room_id: id of the room
         :return: list of {'start_date': ?, 'end_date': }
         """
         return []
+
+    @abc.abstractmethod
+    def make_reservation(self, room_id):
+        """
+        Make a reservation right now. The backend may reject the request
+        :param room_id: id of the room
+        :return: True if the request was accepted, or else False
+        """
+        return False
+
+    @abc.abstractmethod
+    def cancel_reservation(self, room_id):
+        """
+        Cancel the current reservation. The backend may reject the request
+        :param room_id: id of the room
+        :return: True if the request was accepted, or else False
+        """
+        return False
