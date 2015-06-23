bf43278eda7a4545d028e761dce775709351041d
Janos Gyerik <janos@raspberrypi>
2015-06-23 15:34:24 +0200

    rewrote updates-status to use raspberry scripts

--- python/mrgenie/update-status.sh
+++ python/mrgenie/update-status.sh
@@ -8,24 +8,9 @@ status=$(./run.sh mrgenie/status.py $room_id)
 echo room=$room_id
 echo status=$status
 
-BlueLED=17
-GreenLED=22
-RedLED=4
-
-gpio -g mode $BlueLED out
-gpio -g mode $GreenLED out
-gpio -g mode $RedLED out
-
-StatusRed=0
-StatusGreen=0
-StatusBlue=0
-
 case $status in
-    RESERVED) StatusBlue=1 ;;
-    MEETING) StatusRed=1 ;;
-    FREE) StatusGreen=1 ;;
+    RESERVED) update-leds.sh blue ;;
+    MEETING) update-leds.sh red ;;
+    FREE) update-leds.sh green ;;
+    *) update-leds.sh off ;;
 esac
-
-gpio -g write $BlueLED $StatusBlue
-gpio -g write $GreenLED $StatusGreen
-gpio -g write $RedLED $StatusRed
