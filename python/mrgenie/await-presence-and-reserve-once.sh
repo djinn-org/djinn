b83d8639dbc21f759ef9d381cb1d10be8036fd06
Janos Gyerik <janos@kronos>
2015-06-24 09:57:19 +0200

    added status-presence-combo.sh

---
+++ python/mrgenie/await-presence-and-reserve-once.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+# params
+service=$1
+room_id=$2
+await_seconds=$3
+
+# constants
+SwitchGPIO=24
+
+# variables
+presence=0
+await_end=$(($(date +%s) + await_seconds))
+
+waiting() {
+    [[ $(date +%s) -le $await_end ]]
+}
+
+gpio -g mode $SwitchGPIO input
+
+while waiting; do
+    printf .
+    TEST_VAR=$(gpio -g read $SwitchGPIO)
+
+    if [ "$TEST_VAR" = 1 ]; then
+        if [ $presence -eq 0 ]; then
+            echo presence detected, making reservation ...
+            presence=1
+            update-leds.sh red
+            ./run.sh mrgenie/cli.py make-reservation $service $room_id
+            sleep 1
+        else
+            echo leaving room, canceling reservation ...
+            presence=0
+            update-leds.sh green
+            ./run.sh mrgenie/cli.py cancel-reservation $service $room_id
+            echo exit
+            break
+        fi
+    fi
+done
---
+++ python/mrgenie/status-presence-combo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+service=$1
+room_id=$2
+await_seconds=5
+
+# init : sanity test leds
+for color in off red green blue off; do
+    update-leds.sh $color
+done
+
+while :; do
+    ./update-status.sh $service $room_id
+
+    status=$(status.sh $service $room_id)
+    if test $status = FREE; then
+        ./await-presence-and-reserve-once.sh $service $room_id $await_seconds
+    fi
+done
