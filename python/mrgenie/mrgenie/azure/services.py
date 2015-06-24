5ccb99e8348bb3db5431e353432747ab27d187af
Janos Gyerik <janos@kronos>
2015-06-24 11:39:16 +0200

    implemented missing azure methods

--- python/mrgenie/mrgenie/azure/services.py
+++ python/mrgenie/mrgenie/azure/services.py
@@ -5,6 +5,9 @@ import httplib2
 from mrgenie.services import Service
 
 BASEURL = 'https://testinnovathon.azurewebsites.net'
+BASEURL = 'http://192.168.206.211:4017'
+# http://192.168.206.211:4017/api/booking?roomname=R1.1&status=Cancel
+# http://192.168.206.211:4017/api/booking?roomname=R1.1&status=Book
 
 
 def get(path, params=''):
@@ -13,21 +16,19 @@ def get(path, params=''):
         BASEURL + path + '?' + params,
         'GET'
     )
-    return content
+    return json.loads(content.decode())
 
 
 class AzureService(Service):
     def get_rooms(self):
-        rooms = json.loads(get(path='/api/room').decode())
+        rooms = get(path='/api/room')
         return [(x['Name'].strip(), x['Name'].strip()) for x in rooms]
 
     def get_reservations(self, room_id):
-        reservations_json = json.loads(get(
+        reservations_raw = get(
             path='/api/reservation',
             params='roomName={}'.format(room_id)
-        ).decode())
-
-        reservations_raw = reservations_json
+        )
 
         reservations = [
             {
@@ -36,6 +37,14 @@ class AzureService(Service):
             } for x in reservations_raw]
 
         return reservations
+
+    def make_reservation(self, room_id):
+        resp = get(path='/api/booking', params='roomname={}&status=Book'.format(room_id))
+        return resp['Status'] == 'OK'
+
+    def cancel_reservation(self, room_id):
+        resp = get(path='/api/booking', params='roomname={}&status=Cancel'.format(room_id))
+        return resp['Status'] == 'OK' and resp['Id'] == 1
 
 
 def to_date(strdate):
