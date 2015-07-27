c9b107751295c904fb84df5fb2d2d38830145dd3
Janos Gyerik <janos@kronos>
2015-07-27 22:13:57 +0200

    removed route to /reservations/

--- python/django-djinn-backend/api/urls.py
+++ python/django-djinn-backend/api/urls.py
@@ -5,7 +5,7 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r'rooms', views.RoomViewSet)
 router.register(r'rooms/(?P<room_id>[^/.]+)/reservations', views.RoomReservationViewSet)
-router.register(r'reservations', views.ReservationViewSet)
+# router.register(r'reservations', views.ReservationViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
