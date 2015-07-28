0e40187dadd6db389ea56a3b9c256485400eda89
Janos Gyerik <janos@kronos>
2015-07-28 23:48:55 +0200

    validate: start date-time must be before end

--- python/django-djinn-backend/api/serializers.py
+++ python/django-djinn-backend/api/serializers.py
@@ -10,3 +10,13 @@ class RoomSerializer(serializers.ModelSerializer):
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        start = data.get('start')
+        end = data.get('end')
+        if not start < end:
+            raise serializers.ValidationError('start date-time must be before end date-time')
+
+        return data
