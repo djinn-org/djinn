dfa52ce5c37cecb35ca5da7fdb95a939083d3789
Janos Gyerik <janos@kronos>
2015-09-20 23:05:13 +0200

    client admin

--- python/django-djinn-backend/djinn/admin.py
+++ python/django-djinn-backend/djinn/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Building
+from .models import Building, Client
 from .models import Room
 from .models import Equipment
 from .models import RoomEquipment
@@ -19,8 +19,18 @@ class RoomAdmin(admin.ModelAdmin):
 class EquipmentAdmin(admin.ModelAdmin):
     inlines = [RoomEquipmentInline]
 
+
+class ClientAdmin(admin.ModelAdmin):
+    model = Client
+    list_display = ('ip', 'mac', 'alias', 'room', 'is_alive', 'get_last_heartbeat')
+
+    def get_last_heartbeat(self, client):
+        return client.clientheartbeat.last_heartbeat
+    get_last_heartbeat.short_description = 'Last heartbeat'
+
 admin.site.register(Building)
 admin.site.register(Room, RoomAdmin)
 admin.site.register(Equipment, EquipmentAdmin)
 admin.site.register(RoomEquipment)
 admin.site.register(Reservation)
+admin.site.register(Client, ClientAdmin)
