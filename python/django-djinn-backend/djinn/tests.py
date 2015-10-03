c4d75989803ba34a516123510e79a0e8bfe77f34
Janos Gyerik <janos@kronos>
2015-10-03 22:51:15 +0200

    test for keep exact match

--- python/django-djinn-backend/djinn/tests.py
+++ python/django-djinn-backend/djinn/tests.py
@@ -840,7 +840,13 @@ class MergeReservationsTest(TestCase):
         pass
 
     def keep_exact_match(self):
-        pass
+        start = datetime(2015, 9, 25, 18, 30)
+        end = datetime(2015, 9, 25, 19, 0)
+        Reservation.objects.create(room=self.room, start=start, end=end)
+        reservations = create_reservations((start, end))
+        merge_reservations(self.room, reservations)
+        self.assertEquals(1, self.room.reservation_set.count())
+        self.assertEquals(0, self.room.reservationlog_set.count())
 
     def keep_reservation_before(self):
         pass
