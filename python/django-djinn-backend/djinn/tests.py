91e5c8a2dc1a1ea8cd79a0c779b06ae1696d7b20
Janos Gyerik <janos@kronos>
2015-10-04 23:46:44 +0200

    test merge incoming overlaps

--- python/django-djinn-backend/djinn/tests.py
+++ python/django-djinn-backend/djinn/tests.py
@@ -847,7 +847,16 @@ class MergeReservationsTest(TestCase):
         self.assertEquals(0, self.room.reservationlog_set.count())
 
     def add_incoming_overlaps(self):
-        pass
+        start = datetime(2015, 9, 25, 18, 30)
+        end = datetime(2015, 9, 25, 19, 0)
+        reservations = create_reservations((start, end), (start, end))
+        merge_reservations(self.room, reservations)
+        self.assertEquals(2, self.room.reservation_set.count())
+        self.assertEquals(2, self.room.reservationlog_set.count())
+
+        log = self.room.reservationlog_set.first()
+        self.assertEquals(ReservationLog.TYPE_CREATE, log.log_type)
+        self.assertEquals(ReservationLog.TRIGGER_EXT, log.log_trigger)
 
     def keep_exact_match(self):
         start = datetime(2015, 9, 25, 18, 30)
