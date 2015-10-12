0aae0bf0835788a24da61dcc6e16ad706392b35e
Janos Gyerik <janos@kronos>
2015-10-12 22:36:07 +0200

    moved printing saved time logic to helper method

--- python/django-djinn-backend/djinn/management/commands/reservationlogs.py
+++ python/django-djinn-backend/djinn/management/commands/reservationlogs.py
@@ -18,19 +18,13 @@ class Command(BaseCommand):
     def msg(self, message):
         self.stdout.write('* ' + message)
 
-    def handle(self, *args, **options):
-        qs = ReservationLog.objects
-
-        if options['room']:
-            qs = qs.filter(room__external_name=options['room'])
-        if options['day']:
-            day = datetime.strptime(options['day'], '%Y-%m-%d')
-            qs = qs.filter(start__gte=day, end__lt=day + timedelta(days=1))
-
+    def print_saved_time(self, qs):
         cancel_djinn_list = qs.filter(
             log_type=ReservationLog.TYPE_CANCEL,
             log_trigger=ReservationLog.TRIGGER_DJINN
         ).order_by('room', 'log_time')
+
+        total_time_saved = 0
 
         for cancel_djinn in cancel_djinn_list:
             create_ext_list = qs.filter(
@@ -41,7 +35,23 @@ class Command(BaseCommand):
 
             for create_ext in create_ext_list:
                 saved = (create_ext.end - cancel_djinn.log_time).seconds // 60
-                print('{} {} {:%H:%M} {} canceled at {:%H:%M:%S} {}'.format(
+                self.stdout.write('{} {} {:%H:%M} {} canceled at {:%H:%M:%S} {}'.format(
                     saved, create_ext.room.external_name,
                     create_ext.start, create_ext.minutes, cancel_djinn.log_time,
                     cancel_djinn.pk))
+
+                total_time_saved += saved
+
+        self.stdout.write("---")
+        self.stdout.write("total time saved = {}".format(total_time_saved))
+
+    def handle(self, *args, **options):
+        qs = ReservationLog.objects
+
+        if options['room']:
+            qs = qs.filter(room__external_name=options['room'])
+        if options['day']:
+            day = datetime.strptime(options['day'], '%Y-%m-%d')
+            qs = qs.filter(start__gte=day, end__lt=day + timedelta(days=1))
+
+        self.print_saved_time(qs)
