fa851e7916fab4ab55e24027bf575c62db0ef19b
Janos Gyerik <janos@kronos>
2015-10-12 22:18:52 +0200

    more format changes and print room ext name for easier copy-paste

--- python/django-djinn-backend/djinn/management/commands/reservationlogs.py
+++ python/django-djinn-backend/djinn/management/commands/reservationlogs.py
@@ -20,6 +20,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         qs = ReservationLog.objects
+
         if options['room']:
             qs = qs.filter(room__external_name=options['room'])
         if options['day']:
@@ -28,16 +29,19 @@ class Command(BaseCommand):
 
         cancel_djinn_list = qs.filter(
             log_type=ReservationLog.TYPE_CANCEL,
-            log_trigger=ReservationLog.TRIGGER_DJINN).order_by('room', 'log_time')
+            log_trigger=ReservationLog.TRIGGER_DJINN
+        ).order_by('room', 'log_time')
 
         for cancel_djinn in cancel_djinn_list:
             create_ext_list = qs.filter(
                 room=cancel_djinn.room, log_type=ReservationLog.TYPE_CREATE,
                 log_trigger=ReservationLog.TRIGGER_EXT, start=cancel_djinn.start,
-                end=cancel_djinn.end)
+                end=cancel_djinn.end
+            )
 
             for create_ext in create_ext_list:
                 saved = (create_ext.end - cancel_djinn.log_time).seconds // 60
                 print('{} {} {:%H:%m} {} canceled at {:%H:%m:%S} {}'.format(
-                    saved, create_ext.room, create_ext.start, create_ext.minutes, cancel_djinn.log_time,
+                    saved, create_ext.room.external_name,
+                    create_ext.start, create_ext.minutes, cancel_djinn.log_time,
                     cancel_djinn.pk))
